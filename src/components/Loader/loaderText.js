50f505f0f5f163282ea356e1dd66c8cb0a4c4fc9
Amrit-Raj-Sharma <113877472+Amrit-Raj-Sharma@users.noreply.github.com>
2023-05-09 18:49:14 +0530

    Added Loader

---
+++ src/components/Loader/loaderText.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const LoaderText = () => {
+  return (
+    <div class="loader">
+    <span>C</span>
+    <span>O</span>
+    <span>D</span>
+    <span>E</span>
+    <span>:</span>
+    <span>U</span>
+    <span>:</span>
+
+    <span>A</span>
+    <span>P</span>
+    <span>P</span>
+</div>
+  )
+}
+
+export default LoaderText
