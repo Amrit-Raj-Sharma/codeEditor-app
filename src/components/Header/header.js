22f7bf2ad6f152c8a6b446f5ca174aa69a101112
Amrit-Raj-Sharma <113877472+Amrit-Raj-Sharma@users.noreply.github.com>
2023-05-09 18:45:10 +0530

    Updated header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -22,19 +22,49 @@ const themeOptions = [
 const Header = ({ setThemeValue, setTheme, theme }) => {
 
   return (
-    <div className={`header-container ${theme ? 'theme-dark' : 'theme-light'}`}>
-      <div>Logo</div>
-        <>
-          <ThemeSwitcher setTheme={setTheme} theme={theme} />
-        </>
-      <div className='header-right-box'>
-        <select defaultValue='ayu-dark' onChange={(e) => setThemeValue(e.target.value)}>
+
+    <div className={`nav ${theme ? 'theme-dark' : 'theme-light'}`}>
+  <input type="checkbox" id="nav-check" />
+  <div className="nav-header">
+    <div className={`nav-title ${theme ? '' : 'nav-title-light'}`}>
+      Code-U-App
+    </div>
+  </div>
+  <div className="nav-btn">
+    <label htmlFor="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div className={`nav-links ${theme ? '' : 'nav-links-light'}`}>
+    <ThemeSwitcher setTheme={setTheme} theme={theme} />
+
+      
+      <label style={{ fontSize: '14px' }}>Choose Editor Themes </label>
+        <select className={`${theme ? 'theme-dark' : 'theme-light'}`} defaultValue='ayu-dark' onChange={(e) => setThemeValue(e.target.value)}>
           {themeOptions?.map((item, index) => (
             <option key={item+index} value={item}>{item}</option>
-          ))}
+        ))}
         </select>
-      </div>
-    </div>
+        
+  </div>
+</div>
+    
+    // <div className={`header-container ${theme ? 'theme-dark' : 'theme-light'}`}>
+    //   <div>Logo</div>
+    //     <>
+    //       <ThemeSwitcher setTheme={setTheme} theme={theme} />
+    //     </>
+    //   <div className='header-right-box'>
+    //     <select defaultValue='ayu-dark' onChange={(e) => setThemeValue(e.target.value)}>
+    //       {themeOptions?.map((item, index) => (
+    //         <option key={item+index} value={item}>{item}</option>
+    //       ))}
+    //     </select>
+    //   </div>
+    // </div>
   )
 }
 
